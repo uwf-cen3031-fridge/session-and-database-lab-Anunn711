9e4424c8b79891fc8e4232d9fd591fca9304ccec
Anunn711 <ain2@students.uwf.edu>
2024-04-02 10:48:22 -0500

    class work

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -11,11 +11,31 @@ export class AppController {
 
   private initializeRouter() {
 
+    this.router.get("/login"),(req: Request , res:Response) =>{
+      res.render("login");
+    }
+
+    this.router.post("/login"),(req: any, res:Response) =>{
+      req.session.user = req.body.username;
+      res.redirect("/")
+    }
+    //PROTECT THE HOME PAGE
+
+    this.router.use((req : any , res: Response, next) =>{
+      if(req.seesion.user){
+        next();
+      } else {
+        res.redirect("/login");
+      }
+    });
+
     // Serve the home page
-    this.router.get("/", (req: Request, res: Response) => {
+    this.router.get("/", (req: any, res: Response) => {
       try {
         // Render the "home" template as HTML
-        res.render("home");
+        res.render("home",{
+          user : req.session.user
+        });
       } catch (err) {
         this.log.error(err);
       }
